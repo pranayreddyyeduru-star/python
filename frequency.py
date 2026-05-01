db83550e8900848d56b0680758698bb7e5dbd71b
pranayreddyyeduru-star <pranayreddyyeduru@gmail.com>
2026-05-01 18:43:13 -0500

    Update frequency.py

---
+++ frequency.py
@@ -0,0 +1,8 @@
+test_dict = {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coding' : 1}
+K = 2
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res + 1
+print("Frequency of K is : " + str(res))
+
